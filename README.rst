cc912d2a3dbd9cefb5b0304d1080be90ca4c9be7
Samuele Santi <redshadow@hackzine.org>
2013-08-18 19:10:33 +0200

    Initial commit of mezzanine project

---
+++ README.rst
@@ -0,0 +1,9 @@
+Open Data Catalog
+#################
+
+A simple catalog application for Open Data, powered by Mezzanine_,
+a CMS based upon Django_.
+
+.. _Mezzanine: http://mezzanine.jupo.org/
+.. _Django: http://djangoproject.com/
+
---
+++ setup.py
@@ -0,0 +1,31 @@
+## Open Data Catalog (odcatalog)
+## Mezzanine-powered data catalog management application
+##--------------------------------------------------------------------
+
+import sys
+from pkg_resources import normalize_path
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+version = '0.1'
+
+install_requires = [
+    "mezzanine",
+]
+
+setup(
+    name='mezzanine-odcatalog',
+    version=version,
+    packages=find_packages(),
+    url='',
+    license='Apache License, Version 2.0',
+    author='Samuele Santi',
+    author_email='samuele@samuelesanti.com',
+    description='Simple Open Data catalog management application',
+    long_description='Simple Open Data catalog management application',
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+    ],
+    package_data={'': ['README.rst', 'LICENSE']},
+)
